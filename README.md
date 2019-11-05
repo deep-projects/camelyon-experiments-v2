4f0ce045f85ad79b06ad7c9d25a9893b03ddf57f
Christoph Jansen <Christoph.Jansen@htw-berlin.de>
2019-11-05 16:15:44 +0100

    update

--- README.md
+++ README.md
@@ -1,6 +1,76 @@
-# camelyon-experiments-v2
+# CAMELYON Experiments V2
+
+CAMELYON Experiments V2 is an improved and merged version of the previously published [camelyon-slide-to-tiles-experiments](https://github.com/deep-projects/camelyon-slide-to-tiles-experiments) and [camelyon-cnns-training-experiments](https://github.com/deep-projects/camelyon-cnns-training-experiments).
+This version is easier to reproduce and more concise, than the original experiments and has been updated for RED version 8.
+
+This image processing and deep learning pipeline is fully implement as three subsequent experiments in the Reprodcuble Experiment Description (RED) format.
+The experiments can be processed using RED Execution Engines from the [Curious Containers](https://www.curious-containers.cc/) project.
+
+* slide-to-tiles downloads Whole Slide Images (WSI) from the CAMELYON16 dataset via FTP, extracts a certain number of small tiles from the slides and stores the tiles in one HDF5 file per slide.
+* merge-tiles loads a certain number of random tiles from the previously generated HDF5 files and stores them in one large array in a single HDF5 file.
+* cnn-training loads select random batch slices from the merged tiles to train a CNN for tumor classification.
+
+In the original experiment, the complete CAMELYON16 training data set is downloaded and the merge step produces a very large (1.8 TB) tiles file.
+Additionally, the original experiment is executed in the cluster infrastructure of [CBMI - HTW Berlin](https://cbmi.htw-berlin.de/).
+This improved version of the experiment can be easily adapted to use less resources and does not require access to CBMI resources, as long as the user can provide an SSH server with appriate storage space for intermediate and final results.
+The `generate-red.py` script provides convenient parameters to generate custom RED files.
+
+## Resources
+
+These experiments have been built from the following resources.
+
+* Software package: [camelyon-utils 0.5](https://github.com/deep-projects/camelyon-utils/releases/tag/0.5)
+* Docker image: [docker.io/deepprojects/camelyon-utils:0.5](https://cloud.docker.com/u/deepprojects/repository/docker/deepprojects/camelyon-utils) ([Dockerfile](https://github.com/deep-projects/appliances/tree/master/camelyon-utils/0.5))
+* Software package: [camelyon-cnns 0.2](https://github.com/deep-projects/camelyon-utils)
+* Docker image: [docker.io/deepprojects/camelyon-cnns:0.2](https://cloud.docker.com/u/deepprojects/repository/docker/deepprojects/camelyon-cnns) ([Dockerfile](https://github.com/deep-projects/appliances/tree/master/camelyon-cnns/0.2))
+* Docker image with CUDA GPU support: [docker.io/deepprojects/camelyon-cnns:0.2-gpu](https://cloud.docker.com/u/deepprojects/repository/docker/deepprojects/camelyon-cnns) ([Dockerfile](https://github.com/deep-projects/appliances/tree/master/camelyon-cnns/0.2-gpu))
+
+
+## Reproduce
+
+The full experiments, that require a lot of resources, is located in the `cbmi` directory.
+A smaller version of the experiment can generated using the following commands:
 
 ```bash
-./generate-red.py avocado01.f4.htw-berlin.de --cc-agency-url https://agency.f4.htw-berlin.de/cc --output-dir original --tumor-slides-end-index 3 --normal-slides-end-index 3
-faice exec original/slide-to-tiles.red.yml
+pip3 install --user -r requirements
+./generate-red.py avocado01.f4.htw-berlin.de --cc-agency-url https://agency.f4.htw-berlin.de/cc --output-dir custom --tumor-slides-end-index 4 --normal-slides-end-index 4 --num-tiles 256 --gpus 1 --epochs 1
 ```
+
+You should provide a different SSH server host name and `--cc-agency-url`.
+CC-Agency is one of two available RED Execution Engines.
+If you do not want to setup your own instance of CC-Agency, you can remove this parameter entirely.
+The RED Execution Engine will then be set to `ccfaice` to run experiments on your local Linux host.
+This requires Docker to be installed.
+For the usage of GPUs you also need nvidia-container-toolkit or nvidia-docker to be intalled as well.
+You can remove the `--gpus` parameter to fall back to CPU processing.
+
+For more information about the parameters run `./generate-red.py --help`. For more information about Curious Containers we recommend reading the [RED Beginner's Guide](https://www.curious-containers.cc/docs/red-beginners-guide).
+
+To run the generated experiment install the `cc-faice` Python package, that provides the RED client `faice exec` and a local RED execution engine.
+
+```bash
+pip3 install --user pipx
+pipx install cc-faice==8.*
+
+faice exec custom/slide-to-tiles.red.yml
+# wait until succeeded. might be asynchronous with CC-Agency
+
+faice exec custom/merge-tiles.red.yml
+# wait until succeeded. might be asynchronous with CC-Agency
+
+faice exec custom/cnn-training-batchConcurrencyLimit-1.red.yml
+# wait until succeeded. might be asynchronous with CC-Agency
+
+faice exec custom/cnn-training-batchConcurrencyLimit-3.red.yml
+# wait until succeeded. might be asynchronous with CC-Agency
+
+faice exec custom/cnn-training-batchConcurrencyLimit-9.red.yml
+# wait until succeeded. might be asynchronous with CC-Agency
+```
+
+Each of the three `cnn-training-batchConcurrencyLimit-*.red.yml` contains 9 identical CNN training configurations.
+The CNN trainings can be performed with CC-Agency using different batchConcurrencyLimits, such that the 1, 3 or 9 of the 9 runs are executed in parallel.
+Depending on the SSH server, that provides the merged tiles via SSHFS into the experiment containers, you might experience a performance impact resulting in different training times.
+These training times can be inspected in the log files generated on the SSH server by the experiments.
+
+If the `ccfaice` execution engine is used instead of `ccagency`, there will only be one `cnn-training.red.yml` file, because `ccfaice` does not provide parallel execution and therefore does not have a `batchConcurrencyLimit` setting.
--- generate-red.py
+++ generate-red.py
@@ -10,7 +10,8 @@ yaml = YAML(typ='safe')
 
 
 CAMELYON_UTILS_IMAGE = 'docker.io/deepprojects/camelyon-utils:0.5'
-CAMELYON_CNN_IMAGE = 'docker.io/deepprojects/camelyon-cnn:0.1.1'
+CAMELYON_CNN_GPU_IMAGE = 'docker.io/deepprojects/camelyon-cnns:0.2-gpu'
+CAMELYON_CNN_IMAGE = 'docker.io/deepprojects/camelyon-cnns:0.2'
 
 
 parser = ArgumentParser(description='Generate RED files of CAMELYON16 Experiments V2.')
@@ -31,12 +32,20 @@ parser.add_argument(
     help='Number of tiles to be selected per slide for merged.hd5 file. Default: 750, reduce number if resources are limited.'
 )
 parser.add_argument(
-    '--tumor-slides-end-index', action='store', type=int, metavar='INDEX', default=161, choices=range(2, 162),
-    help='Only tumor slides from index position 1 to this end INDEX (exclusive) will be processed. Default: 161.'
+    '--tumor-slides-end-index', action='store', type=int, metavar='INDEX', default=160, choices=range(1, 161),
+    help='Only tumor slides from index position 1 to this end INDEX (inclusive) will be processed. Default: 160.'
 )
 parser.add_argument(
-    '--normal-slides-end-index', action='store', type=int, metavar='INDEX', default=161, choices=range(2, 162),
-    help='Only normal slides from index position 1 to this end INDEX (exclusive) will be processed. Default: 161.'
+    '--normal-slides-end-index', action='store', type=int, metavar='INDEX', default=160, choices=range(1, 161),
+    help='Only normal slides from index position 1 to this end INDEX (inclusive) will be processed. Default: 160.'
+)
+parser.add_argument(
+    '--gpus', action='store', type=int, metavar='NUM',
+    help='NUM of Nvidia GPUs used for cnn-training. Requires nvidia-container-toolkit/nvidia-docker to be installed on the Docker host.'
+)
+parser.add_argument(
+    '--epochs', action='store', type=int, metavar='NUM', default=8,
+    help='NUM of epochs to train CNN. Default: 8'
 )
 args = parser.parse_args()
 
@@ -51,7 +60,7 @@ with open('slide-to-tiles.cwl.yml') as f:
 with open('merge-tiles.cwl.yml') as f:
     merge_tiles_cli = yaml.load(f)
 
-with open('cnn-trainings.cwl.yml') as f:
+with open('cnn-training.cwl.yml') as f:
     cnn_training_cli = yaml.load(f)
 
 
@@ -66,12 +75,12 @@ execution_ccfaice = {
 pixel_spacing = 0.0002439
 ssh_experiments_dir = 'camelyon-experiments-v2'
 ssh_tiles_dir = '{}/tiles'.format(ssh_experiments_dir)
-ftp_camelyon_url = 'ftp://parrot.genomics.cn/gigadb/pub/10.5524/100001_101000/100439/CAMELYON16/training/'
+ftp_camelyon_url = 'ftp://parrot.genomics.cn/gigadb/pub/10.5524/100001_101000/100439/CAMELYON16/training'
 
 
 batches = []
 
-index = range(1, args.tumor_slides_end_index)
+index = range(1, args.tumor_slides_end_index+1)
 
 for i in index:
     batch = {
@@ -120,7 +129,7 @@ for i in index:
 
 # normal_086.tif does not exist in dataset
 # normal_144.tif cannot be processed
-index = [i for i in range(1, args.normal_slides_end_index) if i not in [86, 144]]
+index = [i for i in range(1, args.normal_slides_end_index+1) if i not in [86, 144]]
 
 for i in index:
     batch = {
@@ -180,9 +189,7 @@ red = {
     'container': {
         'engine': 'docker',
         'settings': {
-            'image': {
-                'url': CAMELYON_UTILS_IMAGE
-            },
+            'image': {'url': CAMELYON_UTILS_IMAGE},
             'ram': 8192
         }
     },
@@ -225,13 +232,14 @@ red = {
             'class': 'Directory',
             'connector': {
                 'command': 'red-connector-ssh',
+                'mount': True,
                 'access': {
                     'host': args.ssh_host,
                     'auth': {
                         'username': '{{ssh_username}}',
                         'password': '{{ssh_password}}'
                     },
-                    'filePath': ssh_tiles_dir
+                    'dirPath': ssh_tiles_dir
                 }
             }
         },
@@ -239,13 +247,15 @@ red = {
             'class': 'Directory',
             'connector': {
                 'command': 'red-connector-ssh',
+                'mount': True,
                 'access': {
                     'host': args.ssh_host,
                     'auth': {
                         'username': '{{ssh_username}}',
                         'password': '{{ssh_password}}'
                     },
-                    'filePath': ssh_tiles_dir
+                    'dirPath': ssh_merged_dir,
+                    'writable': True
                 }
             }
         },
@@ -255,9 +265,7 @@ red = {
     'container': {
         'engine': 'docker',
         'settings': {
-            'image': {
-                'url': CAMELYON_UTILS_IMAGE
-            },
+            'image': {'url': CAMELYON_UTILS_IMAGE},
             'ram': 8192
         }
     },
@@ -270,3 +278,146 @@ if args.output_dir:
 
 with open(red_path, 'w') as f:
     yaml.dump(red, f)
+
+
+# generate cnn-training.red.yml
+ssh_training_dir = '{}/training'.format(ssh_experiments_dir)
+
+container = {
+    'engine': 'docker',
+    'settings': {
+        'image': {'url': CAMELYON_CNN_IMAGE},
+        'ram': 32768
+    }
+}
+
+if args.gpus:
+    container = {
+        'engine': 'docker',
+        'settings': {
+            'image': {'url': CAMELYON_CNN_GPU_IMAGE},
+            'ram': 32768,
+            'gpus': {
+                'vendor': 'nvidia',
+                'count': args.gpus
+            }
+        }
+    }
+
+
+def generate_batch(lim=None, run=None):
+    inputs = {
+        'hdf5': {
+            'class': 'Directory',
+            'connector': {
+                'command': 'red-connector-ssh',
+                'mount': True,
+                'access': {
+                    'host': args.ssh_host,
+                    'auth': {
+                        'username': '{{cbmi_username}}',
+                        'password': '{{cbmi_password}}',
+                    },
+                    'dirPath': ssh_merged_dir
+                }
+            }
+        },
+        'arch': '1',
+        'fast_hdf5': '1',
+        'mask_threshold': '0.1',
+        'epochs': '{}'.format(args.epochs),
+        'queue_size': '1'
+    }
+
+    results_dir = ssh_training_dir
+    if lim is not None:
+        results_dir = os.path.join(results_dir, 'lim_{}'.format(lim))
+
+    if run is not None:
+        results_dir = os.path.join(results_dir, 'run_{}'.format(run))
+
+    outputs = {}
+    for key, file_name in [
+        ('log', 'log.json'),
+        ('acc_plot', 'acc.png'),
+        ('loss_plot', 'loss.png'),
+        ('roc_plot', 'roc.png'),
+        ('model_final', 'model_final.hdf5'),
+        ('checkpoint', 'checkpoint'),
+        ('model_checkpoint_data', 'model_checkpoint.ckpt.data'),
+        ('model_checkpoint_index', 'model_checkpoint.ckpt.index')
+    ]:
+        outputs[key] = {
+            'class': 'File',
+            'connector': {
+                'command': 'red-connector-ssh',
+                'access': {
+                    'host': args.ssh_host,
+                    'auth': {
+                        'username': '{{cbmi_username}}',
+                        'password': '{{cbmi_password}}',
+                    },
+                    'filePath': os.path.join(results_dir, file_name)
+                }
+            }
+        }
+
+    return {
+        'inputs': inputs,
+        'outputs': outputs
+    }
+
+
+if args.cc_agency_url:
+    for lim in [1, 3, 9]:
+        batches = []
+        for run in [str(i) for i in range(1, 10)]:
+            batches.append(generate_batch(lim=lim, run=run))
+
+        execution = {
+            'engine': 'ccagency',
+            'settings': {
+                'access': {
+                    'url': args.cc_agency_url,
+                    'auth': {
+                        'username': '{{agency_username}}',
+                        'password': '{{agency_password}}'
+                    }
+                },
+                'retryIfFailed': True,
+                'batchConcurrencyLimit': lim
+            }
+        }
+        red = {
+            'redVersion': '8',
+            'cli': cnn_training_cli,
+            'batches': batches,
+            'container': container,
+            'execution': execution
+        }
+
+        red_path = 'cnn-training-batchConcurrencyLimit-{}.red.yml'.format(lim)
+        if args.output_dir:
+            red_path = os.path.join(args.output_dir, red_path)
+
+        with open(red_path, 'w') as f:
+            yaml.dump(red, f)
+
+else:
+    batch = generate_batch()
+
+    red = {
+        'redVersion': '8',
+        'cli': cnn_training_cli,
+        'inputs': batch['inputs'],
+        'outputs': batch['outputs'],
+        'container': container,
+        'execution': execution_ccfaice
+    }
+
+    red_path = 'cnn-training.red.yml'
+    if args.output_dir:
+        red_path = os.path.join(args.output_dir, red_path)
+
+    with open(red_path, 'w') as f:
+        yaml.dump(red, f)
